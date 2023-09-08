fa3c09197713cff62c8f40db1da2393992e41f74
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-09-08 08:57:02 +0600

    Leetcode 14 v2

--- LeetCode/CSh/Easy/LongestCommonPrefix_14.cs
+++ LeetCode/CSh/Easy/LongestCommonPrefix_14.cs
@@ -6,7 +6,7 @@ namespace CSh.Easy
     public class LongestCommonPrefix_14
     {
         // 91ms 40.22MB
-        public string LongestCommonPrefix(string[] strs)
+        public string LongestCommonPrefix_v1(string[] strs)
         {
             StringBuilder result = new StringBuilder();
             int minLength = strs.Min(str => str.Length);
@@ -30,6 +30,22 @@ namespace CSh.Easy
                 else { break; }
             }
             return result.ToString();
+        }
+
+        // 88ms 40.70MB
+        public string LongestCommonPrefix_v2(string[] strs)
+        {            
+            int minLength = strs.Min(str => str.Length);
+            string result = strs.FirstOrDefault(str => str.Length == minLength);
+
+            for (int i = 0; i < strs.Length; ++i)
+            {
+                while (!strs[i].StartsWith(result))
+                {
+                    result = result.Remove(result.Length - 1);
+                }
+            }
+            return result;
         }
     }
 }
