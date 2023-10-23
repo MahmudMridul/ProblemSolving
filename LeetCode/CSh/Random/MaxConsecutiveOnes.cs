e32bc65c3263d8c56a8795976db6d4fd46984ec7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-23 14:45:20 +0600

    Array 101 leetcode problems

---
+++ LeetCode/CSh/Random/MaxConsecutiveOnes.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace CSh.Random
+{
+    internal class MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0, max = 0;
+            for(int i = 0; i < nums.Length; ++i) 
+            {
+                if (nums[i] == 1)
+                {
+                    ++count;
+                }
+                else
+                {
+                    max = Math.Max(max, count);
+                    count = 0;
+                }
+            }
+            max = Math.Max(max, count);
+            return max;
+        }
+    }
+}
---
+++ LeetCode/CSh/Random/NumberWithEvenDigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Random
+{
+    internal class NumberWithEvenDigits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            return nums.Count(num => num.ToString().Length % 2 == 0);
+        }
+
+        public int[] SortedSquares(int[] nums)
+        {
+            return nums.Select(num => num * num).ToArray();
+        }
+    }
+}
