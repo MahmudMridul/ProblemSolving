36894745f0082f47598fa438b747266a5e7c6461
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-27 21:14:08 +0600

    Continue leetcode array 101

---
+++ LeetCode/CSh/Random/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Random
+{
+    internal class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int pointer = 1;
+            int lastSeen = nums[0];
+            for(int i = 1; i < nums.Length; ++i)
+            {
+                if (nums[i] != lastSeen)
+                {
+                    nums[pointer] = nums[i];
+                    ++pointer;
+                    lastSeen = nums[i];
+                }
+            }
+            return pointer;
+        }
+    }
+}
---
+++ LeetCode/CSh/Random/RemoveFromArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Random
+{
+    internal class RemoveFromArray
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int pointer = 0;
+            for(int i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] != val)
+                {
+                    (nums[pointer], nums[i]) = (nums[i], nums[pointer]);
+                    ++pointer;
+                }
+            }
+            return pointer;
+        }
+    }
+}
