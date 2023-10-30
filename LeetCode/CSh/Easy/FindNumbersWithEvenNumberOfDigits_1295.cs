43105b98221f1fa2cab0551ae9e63b6e1a635597
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 11:25:50 +0600

    Add Find Numbers With Event Number Of Digits 1295

---
+++ LeetCode/CSh/Easy/FindNumbersWithEvenNumberOfDigits_1295.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class FindNumbersWithEvenNumberOfDigits_1295
+    {
+        public int FindNumbers(int[] nums)
+        {
+            return nums.Count(num => num.ToString().Length % 2 == 0);
+        }
+
+
+    }
+}
