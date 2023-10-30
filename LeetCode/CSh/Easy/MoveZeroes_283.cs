6dd3378820a002390d39dd8dee62528a603ab166
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 12:23:39 +0600

    Add Move Zeroes 283

---
+++ LeetCode/CSh/Easy/MoveZeroes_283.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class MoveZeroes_283
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int pointer = 0;
+            for(int i =  0; i < nums.Length; ++i)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[pointer] = nums[i];
+                    ++pointer;
+                }
+            }
+            while(pointer < nums.Length)
+            {
+                nums[pointer] = 0;
+                ++pointer;
+            }
+        }
+    }
+}
