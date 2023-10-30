62fa36f7f7a79efd6e6fb58f346b50d3d2e79023
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 11:48:58 +0600

    Add Replace Elements With Greatest On Right Side 1299

---
+++ LeetCode/CSh/Easy/ReplaceElementsWithGreatestOnRightSide_1299.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class ReplaceElementsWithGreatestOnRightSide_1299
+    {
+        public int[] ReplaceElements_v2(int[] arr)
+        {
+            int max = arr[arr.Length - 1];
+            for(int i = arr.Length - 2; i >= 0; --i)
+            {
+                int temp = arr[i];
+                arr[i] = max;
+                max = Math.Max(max, temp);
+            }
+            arr[arr.Length - 1] = -1;
+            return arr;
+        }
+
+        public int[] ReplaceElements_v1(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; ++i)
+            {
+                arr[i] = FindMax(arr, i + 1);
+            }
+            arr[arr.Length - 1] = -1;
+            return arr;
+        }
+
+        private int FindMax(int[] arr, int start)
+        {
+            return arr.Skip(start).Max();
+        }
+    }
+}
