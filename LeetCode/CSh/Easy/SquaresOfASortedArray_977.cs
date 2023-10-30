105b1c25c4136547ec38ca66c9f28958d4612ca0
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 11:27:15 +0600

    Add Squares Of A Sorted Array 977

---
+++ LeetCode/CSh/Easy/SquaresOfASortedArray_977.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class SquaresOfASortedArray_977
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            return nums.Select(num => num * num).ToArray();
+        }
+    }
+}
