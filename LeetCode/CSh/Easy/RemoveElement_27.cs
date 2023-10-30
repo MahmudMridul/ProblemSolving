598dd5a62045f2276b0a9df2d22d6a3fa09524e2
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 11:26:52 +0600

    Add Remove Element 27

---
+++ LeetCode/CSh/Easy/RemoveElement_27.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class RemoveElement_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int pointer = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] != val)
+                {
+                    (nums[pointer], nums[i]) = (nums[i], nums[pointer]);
+                    ++pointer;
+                }
+            }
+            return pointer;
+        }
+    }
+}
