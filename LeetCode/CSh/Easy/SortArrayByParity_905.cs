1f838d754d6535ffecf29d601ce3f7b104567212
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 12:23:59 +0600

    Add Sort Array By Parity 905

---
+++ LeetCode/CSh/Easy/SortArrayByParity_905.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class SortArrayByParity_905
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            return nums.OrderBy(n => n % 2 != 0).ToArray();
+        }
+    }
+}
