47d419885b22676a807f888c9ce94e207161914b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-10-30 10:31:54 +0600

    Add ValidMountainArray 941

---
+++ LeetCode/CSh/Easy/ValidMountainArray_941.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class ValidMountainArray_941
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            if(arr.Length < 3) return false;
+            int maxIndex = Array.IndexOf(arr, arr.Max());
+            if(maxIndex == 0 || maxIndex == arr.Length - 1) return false;
+            
+            for(int i = maxIndex; i > 0; --i)
+            {
+                if (arr[i] <= arr[i - 1]) return false;
+            }
+            for (int i = maxIndex; i < arr.Length - 1; ++i)
+            {
+                if (arr[i] <= arr[i + 1]) return false;
+            }
+            return true;
+        }
+    }
+}
