d878680adb18c75063891953e4dcd8f6e35db24a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-06 08:43:08 +0600

    Update Height checker 1051

--- LeetCode/CSh/Easy/HeightChecker_1051.cs
+++ LeetCode/CSh/Easy/HeightChecker_1051.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,16 @@ namespace CSh.Easy
 {
     internal class HeightChecker_1051
     {
-         public int HeightChecker(int[] heights)
-         {
+        public int HeightChecker(int[] heights)
+        {
             int[] copy = (int[])heights.Clone();
             Array.Sort(copy);
             int count = 0;
             for (int i = 0; i < heights.Length; ++i)
             {
-                if (heights[i] != copy[i]) ++count; 
+                if (heights[i] != copy[i]) ++count;
             }
             return count;
-         }
+        }
     }
 }
