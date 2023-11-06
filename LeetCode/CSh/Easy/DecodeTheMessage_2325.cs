ad8151367641031a3bdc0f8585ada1b22adb02ae
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-06 11:02:45 +0600

    Add Decode the message 2325

---
+++ LeetCode/CSh/Easy/DecodeTheMessage_2325.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class DecodeTheMessage_2325
+    {
+        public string DecodeMessage(string key, string message)
+        {
+            Dictionary<char, char> map = new Dictionary<char, char>();
+            char k = 'a';
+            for(int i = 0; i < key.Length; ++i)
+            {
+                if (key[i] != ' ' && !map.ContainsKey(key[i]))
+                {
+                    map[key[i]] = k;
+                    ++k;
+                }
+                if (map.Count >= 26) break;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < message.Length; ++i)
+            {
+                if (message[i] == ' ')
+                {
+                    sb.Append(' ');
+                }
+                else
+                {
+                    sb.Append(map[message[i]]);
+                }
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
