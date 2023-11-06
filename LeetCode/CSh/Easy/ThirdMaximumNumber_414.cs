89ab8097862a98e537ac00eab101e73b320fff5a
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-06 08:43:57 +0600

    Add Third max number 414

---
+++ LeetCode/CSh/Easy/ThirdMaximumNumber_414.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class ThirdMaximumNumber_414
+    {
+        public int ThirdMax(int[] nums)
+        {
+            int[] val = nums
+                    .Distinct()
+                    .OrderBy(n => n)
+                    .TakeLast(3)
+                    .ToArray();
+            return val.Length < 3 ? val[val.Length - 1] : val[0];
+        }
+    }
+}
