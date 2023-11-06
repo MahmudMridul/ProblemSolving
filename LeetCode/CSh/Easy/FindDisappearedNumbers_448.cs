531321fc477f5e643c01c6e8861a7e39c754d239
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-06 08:43:27 +0600

    Add Third max number 414

---
+++ LeetCode/CSh/Easy/FindDisappearedNumbers_448.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSh.Easy
+{
+    internal class FindDisappearedNumbers_448
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (Array.BinarySearch(nums, i + 1) < 0)
+                {
+                    result.Add(i + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
