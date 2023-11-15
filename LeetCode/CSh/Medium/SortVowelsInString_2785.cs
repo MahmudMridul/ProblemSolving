fc2e445066254265767dfd4b6fd9d1ece0b6a22f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-11-15 12:00:20 +0600

    Add SortVowelsInString_2785

---
+++ LeetCode/CSh/Medium/SortVowelsInString_2785.cs
@@ -0,0 +1,34 @@
+﻿
+namespace CSh.Medium
+{
+    public class SortVowelsInString_2785
+    {
+        public string SortVowels(string s)
+        {
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+            char[] chars = s.ToCharArray();
+            List<int> indexes = new List<int>();
+            List<char> vowelChars = new List<char>();
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                char c = s[i];
+                if (vowels.Contains(c))
+                {
+                    indexes.Add(i);
+                    vowelChars.Add(c);
+                }
+            }
+            if (vowelChars.Count == 0) return s;
+            vowelChars.Sort();
+
+            int p = 0;
+            for (int i = 0; i < indexes.Count; ++i)
+            {
+                chars[indexes[i]] = vowelChars[p];
+                ++p;
+            }
+            return string.Join("", chars);
+        }
+    }
+}
